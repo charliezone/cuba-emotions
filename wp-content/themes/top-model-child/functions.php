dee0bd5fdc1ed6a600704add656ea3309099b029
Carlos Rafael Gonzalez Perdomo <carlos.g@maniak.com.mx>
2020-03-18 15:20:17 -0400

    revolutions slider order by likes

--- wp-content/themes/top-model-child/functions.php
+++ wp-content/themes/top-model-child/functions.php
@@ -15,4 +15,28 @@ function top_model_child_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'top_model_child_enqueue_styles', 11);
+
+function modify_slider_order($query, $slider_id) {
+ 
+    // only alter the order for slider with "x" ID
+    // http://tinyurl.com/zb6hzpc
+    if($slider_id == 2 || $slider_id == 3) {
+ 
+        // Custom Meta key/name
+        $query['meta_key'] = 'cmsmasters_likes';
+ 
+        // Order by Custom Meta values
+        $query['orderby'] = 'cmsmasters_likes';
+ 
+        // Calculate order based on:
+        // 'NUMERIC', 'CHAR', 'DATE', 'DATETIME', 'TIME'
+        $query['meta_type'] = 'NUMERIC';
+ 
+    }
+ 
+    return $query;
+ 
+}
+ 
+add_filter('revslider_get_posts', 'modify_slider_order', 10, 2);
 ?>
