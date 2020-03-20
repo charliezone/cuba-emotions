ff04497d3745486dc5e0b10d675723944e311583
Carlos Rafael Gonzalez Perdomo <carlos.g@maniak.com.mx>
2020-03-20 16:29:46 -0400

    cpt list shortcode

--- wp-content/plugins/cuba-emotions/cuba-emotions.php
+++ wp-content/plugins/cuba-emotions/cuba-emotions.php
@@ -100,7 +100,7 @@ function ce_carousel( $atts, $content = null ) {
         array(
             'taxonomy' => 'pl-categs',
             'field'    => 'slug',
-            'terms'    => 'women',
+            'terms'    => $a['category_name'],
         ),
     )
   );
@@ -143,3 +143,98 @@ function ce_carousel( $atts, $content = null ) {
 }
 
 add_shortcode( 'ce_carousel', 'ce_carousel' );
+
+function ce_cpt_list( $atts, $content = null ) {
+  wp_enqueue_style( 'ce-app-style' );
+
+  $a = shortcode_atts( array(
+     'post_type' => 'profile',
+     'posts_per_page' => '6',
+     'category_name' => 'women'
+  ), $atts );
+  
+  ob_start();
+  
+  $args = array( 
+     'post_type' => $a['post_type'],
+     'posts_per_page' => $a['posts_per_page'],
+     'tax_query' => array(
+        array(
+            'taxonomy' => 'pl-categs',
+            'field'    => 'slug',
+            'terms'    => $a['category_name'],
+        ),
+      ),
+      'paged' => $paged
+  );
+
+  if (get_query_var('paged')) { 
+		$paged = get_query_var('paged'); 
+	} elseif (get_query_var('page')) { 
+		$paged = get_query_var('page'); 
+	} else { 
+		$paged = 1; 
+	}
+	
+	
+	$args['paged'] = $paged;
+     
+  $posts_query = new WP_Query;
+  $posts_query->query( $args );
+  if ($posts_query->have_posts()) {
+     ?>
+      <div class="ce-cpt-profile-container cmsmasters_profile horizontal">
+        <?php
+          while($posts_query->have_posts()){
+            $posts_query->the_post();?>
+            <article class="ce-cpt-profile cmsmasters_profile_horizontal one_third profile type-profile <?php if (has_post_thumbnail()): ?>has-post-thumbnail <?php endif ?> hentry">
+              <?php if (has_post_thumbnail()): ?>
+                <div class="profile_outer">
+                  <div class="cmsmasters_profile_img_wrap">
+                    <figure class="cmsmasters_img_rollover_wrap preloader">
+                      <img width="300" height="300" src="<?php the_post_thumbnail_url('medium') ?>" class="full-width wp-post-image" alt="<?php the_title() ?>" title="<?php the_title() ?>" srcset="<?php the_post_thumbnail_url('medium') ?> 300w, <?php the_post_thumbnail_url('thumbnail') ?> 150w, <?php the_post_thumbnail_url('thumbnail') ?> 70w" sizes="(max-width: 300px) 100vw, 300px">
+                        <div class="cmsmasters_img_rollover">
+                          <a href="<?php the_permalink() ?>" title="<?php the_title() ?>" class="cmsmasters_open_post_link">
+                            <span>
+                              <span>View</span><span>profile</span>
+                            </span>
+                          </a>
+                        </div>
+                      </figure>
+                  </div>
+                <?php endif ?>  
+                  <div class="profile_inner">
+                    <header class="cmsmasters_profile_header entry-header">
+                      <h3 class="cmsmasters_profile_title entry-title">
+                        <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                      </h3>
+                    </header>
+                  </div>
+            </article>
+
+        <?php } ?>
+        <div class="ce-pagination">
+
+          <?php
+            echo cmsmasters_pagination($posts_query->max_num_pages)
+          ?>
+
+        </div>
+
+        <?php
+          wp_reset_postdata();
+        ?>
+          </div>
+        <?php
+     /* Get the buffered content into a var */
+     $news = ob_get_contents();
+
+     /* Clean buffer */
+     ob_end_clean();
+     return $news;
+  }else{
+     return 'No posts found';
+  }
+}
+
+add_shortcode( 'ce_cpt_list', 'ce_cpt_list' );
